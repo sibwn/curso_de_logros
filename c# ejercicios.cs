bd4654c9f36c330fbdbee1c1c743123867542912
sibwn <Santiagosalas512@gmail.com>
2025-03-26 17:32:42 -0400

    C# ejercicios.cs

---
+++ c# ejercicios.cs
@@ -0,0 +1,109 @@
+using System;
+
+public class AreaTriangulo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("ingrese la base del triangulo: ");
+        double baseTriangulo =
+        Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("ingrese la altura del triangulo: ");
+        double alturaTriangulo = 
+        Convert.ToDouble(Console.ReadLine());
+        
+        double area = (baseTriangulo * alturaTriangulo) / 2;
+        
+         Console.WriteLine ($"El area del triangulo es: {area}");
+    }
+}
+
+using System;
+
+public class AreaTriangulo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("ingrese los grados celsius: ");
+        double celsius = 
+        Convert.ToDouble(Console.ReadLine());
+        
+    
+        
+        double gradoFahrenheit = (celsius * 9 / 5) + 32;
+        
+         Console.WriteLine ($"gradoFahrenheit es igual a:{gradoFahrenheit} ");
+    }
+}
+
+using System;
+
+public class AreaTriangulo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("ingrese el primer numero: ");
+        double num1 =
+        Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("ingrese el segundo numero: ");
+        double num2 = 
+        Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("ingrese el tercer numero: ");
+        double num3 = 
+        Convert.ToDouble(Console.ReadLine());
+        
+        double promedio = (num1 + num2 + num3) / 3;
+        
+         Console.WriteLine ($"El promedio de los 3 numeros es: {promedio}");
+    }
+}
+
+using System;
+
+public class AreaTriangulo
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("ingrese el dividendo: ");
+        double dividendo =
+        Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("ingrese el divisor: ");
+        double divisor = 
+        Convert.ToDouble(Console.ReadLine());
+        
+        double modulo = (dividendo % divisor)  ;
+        
+         Console.WriteLine ($"El resto de la division es: {modulo}");
+    }
+}
+
+using System;
+
+public class AreaTriangulo
+{
+    public static void Main(string[] args)
+    {
+   int num;
+   num = 2;
+   int num2;
+   num2 = 4;
+   Console.Write(num + num2);
+    }
+}
+
+using System;
+
+public class AreaTriangulo
+{
+    public static void Main(string[] args)
+    {
+   int num;
+   num = 7;
+   int num2;
+   num2 = 4;
+   Console.Write(num - num2);
+    }
+}
